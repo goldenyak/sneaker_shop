a75c4ed4a4fd4102c67a7cf10de0253f90ea91cf
goldenyak <goldenyakegor@gmail.com>
2021-09-27 01:27:39 +0300

    layout fixes

--- my-app/src/components/EmptyCart.js
+++ my-app/src/components/EmptyCart.js
@@ -1,13 +1,13 @@
 function EmptyCart({cartOpened}) {
     return (
-        <div className="empty_cart d-flex flex-column align-center ">
+        <div className="empty_cart d-flex flex-column align-center justify-between ">
 
             <img width={120} height={120} src='/img/empty-cart.png' alt='Empty'/>
             <h2>Корзина пустая</h2>
             <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
-            <div onClick={cartOpened} className="backspase_button d-flex align-center justify-between mt-40">
+            <div onClick={cartOpened} className="backspase_button d-flex align-center justify-between mt-30">
                 <img width={14} height={12} src='/icons/arrow.svg' alt='Arrow'/>
-                <p>Вернуться назад</p>
+                <p className="backspase_button_text">Вернуться назад</p>
             </div>
 
         </div>
